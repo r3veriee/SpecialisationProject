b107042b55f24896ccfaa0896173a88261e6f1d9
Rizq Rayyan <231063F@mymail.nyp.edu.sg>
2026-06-30 15:22:21 +0800

    wk 10 extra 2
    fixed turret

--- Assets/Scripts/TurretHazard.cs
+++ Assets/Scripts/TurretHazard.cs
@@ -4,7 +4,7 @@ public class TurretHazard : MonoBehaviour
 {
     public Transform player;
     public float range = 50f;
-    public float chargeTime = 3f; // Faster for your fast-paced game
+    public float chargeTime = 3f;
     public LayerMask visionBlockers;
 
     private float currentCharge = 0f;
@@ -14,6 +14,7 @@ public class TurretHazard : MonoBehaviour
     {
         if (player == null) player = GameObject.FindGameObjectWithTag("Player").transform;
         laserSight = GetComponent<LineRenderer>();
+        if (laserSight != null) laserSight.enabled = false;
     }
 
     void Update()
@@ -31,24 +32,34 @@ public class TurretHazard : MonoBehaviour
 
             if (laserSight != null)
             {
+                // Turn the laser on when it spots the player
+                laserSight.enabled = true;
+
                 laserSight.SetPosition(0, transform.position);
                 laserSight.SetPosition(1, player.position);
                 laserSight.startColor = Color.Lerp(Color.yellow, Color.red, currentCharge / chargeTime);
+                laserSight.endColor = laserSight.startColor; // Make sure the whole beam changes color
             }
 
             if (currentCharge >= chargeTime) Fire();
         }
         else
         {
-            currentCharge = 0f; // Reset if line of sight is broken!
-            if (laserSight != null) laserSight.SetPosition(1, transform.position);
+            currentCharge = 0f; // Reset charge if line of sight is broken
+
+            // Turn the laser completely off when the player hides
+            if (laserSight != null) laserSight.enabled = false;
         }
     }
 
     void Fire()
     {
+        // Instakill the player
         CheckpointSystem cp = player.GetComponent<CheckpointSystem>();
         if (cp != null) cp.Respawn();
+
+        // Reset the turret
         currentCharge = 0f;
+        if (laserSight != null) laserSight.enabled = false;
     }
 }
