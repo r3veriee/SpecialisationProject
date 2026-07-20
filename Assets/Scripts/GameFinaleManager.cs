c6a6fb8e725a30f320f96ba307cf235d2d4bc6bb
RizqRayyanNYP <231063F@mymail.nyp.edu.sg>
2026-07-21 04:08:34 +0800

    almost done

--- Assets/Scripts/GameFinaleManager.cs
+++ Assets/Scripts/GameFinaleManager.cs
@@ -4,15 +4,11 @@ using System.Collections;
 
 public class GameFinaleManager : MonoBehaviour
 {
-    [Header("Cinematic Cameras")]
     public Camera mainPlayerCam;
-    [Tooltip("A new camera placed high above the tower looking straight down.")]
-    public Camera topDownCinematicCam;
-    [Tooltip("An empty GameObject in the exact center of the tower. We will spin this!")]
+    public Camera orbitCinematicCam;
     public Transform cameraPivot;
     public float cameraSpinSpeed = 25f;
 
-    [Header("Visuals & UI")]
     public CanvasGroup blackFadeScreen;
     public TextMeshProUGUI thankYouText;
     public float timeBeforeFadeStarts = 2f;
@@ -22,9 +18,8 @@ public class GameFinaleManager : MonoBehaviour
 
     void Start()
     {
-        if (topDownCinematicCam != null) topDownCinematicCam.enabled = false;
+        if (orbitCinematicCam != null) orbitCinematicCam.enabled = false;
 
-        // Hide the UI at the start
         if (thankYouText != null)
         {
             thankYouText.gameObject.SetActive(false);
@@ -45,52 +40,49 @@ public class GameFinaleManager : MonoBehaviour
     {
         finaleTriggered = true;
 
-        // STOP THE GLOBAL TIMER
-        if (TimeTrialManager.Instance != null)
-        {
-            TimeTrialManager.Instance.StopTimer();
-        }
+        if (TimeTrialManager.Instance != null) TimeTrialManager.Instance.StopTimer();
 
-        // Hide the player completely
         player.SetActive(false);
 
-        // Swap to the spinning camera
         if (mainPlayerCam != null) mainPlayerCam.enabled = false;
-        if (topDownCinematicCam != null)
+        if (orbitCinematicCam != null)
         {
-            topDownCinematicCam.enabled = true;
-            topDownCinematicCam.transform.SetParent(cameraPivot);
+            orbitCinematicCam.enabled = true;
+            orbitCinematicCam.transform.SetParent(cameraPivot);
         }
 
-        // Trigger the Chromatic burst
         ChromaticDecay decayScript = FindObjectOfType<ChromaticDecay>();
         if (decayScript != null) decayScript.TriggerColourBurst(5f);
 
-        // Spin while letting them look at the tower
         float timer = 0f;
         while (timer < timeBeforeFadeStarts)
         {
-            if (cameraPivot != null) cameraPivot.Rotate(Vector3.up, cameraSpinSpeed * Time.deltaTime);
+            if (cameraPivot != null)
+            {
+                cameraPivot.Rotate(Vector3.up, cameraSpinSpeed * Time.deltaTime);
+                if (orbitCinematicCam != null) orbitCinematicCam.transform.LookAt(cameraPivot.position);
+            }
             timer += Time.deltaTime;
             yield return null;
         }
 
-        // Slowly fade to black while still spinning
         float alpha = 0f;
         while (alpha < 1f)
         {
-            if (cameraPivot != null) cameraPivot.Rotate(Vector3.up, cameraSpinSpeed * Time.deltaTime);
+            if (cameraPivot != null)
+            {
+                cameraPivot.Rotate(Vector3.up, cameraSpinSpeed * Time.deltaTime);
+                if (orbitCinematicCam != null) orbitCinematicCam.transform.LookAt(cameraPivot.position);
+            }
             alpha += Time.deltaTime * fadeSpeed;
             if (blackFadeScreen != null) blackFadeScreen.alpha = alpha;
             yield return null;
         }
 
-        // Fade in the "Thank You" text AND the Final Score
         if (thankYouText != null)
         {
             thankYouText.gameObject.SetActive(true);
 
-            // Append the final time from the UI manager onto the end screen
             if (TimeTrialManager.Instance != null)
             {
                 thankYouText.text = "THANK YOU FOR PLAYING\n\nFINAL TIME: " + TimeTrialManager.Instance.timerText.text;
